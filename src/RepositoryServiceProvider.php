f5c3d98ebf6740cbbe9f4528b895a473046949f7
Zaman Shovon <zaman.monir20188@gmail.com>
2024-10-26 13:35:01 +0600

    Initial composer setup done for package

---
+++ src/RepositoryServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Zaman\Repo;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        //code ....
+    }
+
+    public function register()
+    {
+        //code ....
+    }
+
+}
